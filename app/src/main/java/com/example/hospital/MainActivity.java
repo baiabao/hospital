0b4bb297b1b9663f50b70863a087e852478addef
apple <apple@applede-MacBook-Air.local>
2019-06-02 14:11:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hospital/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.hospital;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button res = (Button)findViewById(R.id.reserve);
+        Button sch = (Button) findViewById(R.id.schedule);
+
+
+        sch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,sch.class);
+                startActivity(intent);
+
+            }
+        });
+
+        res.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,res.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+
+}
